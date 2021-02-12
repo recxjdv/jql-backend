c759ef929e89b48dcef24dc2e8ce1b5cd36e88b3
James Vaughan <jdv@.venet.org>
2021-02-12 11:24:50 +0000

    Adding in route tests

---
+++ test/lookup.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+
+const app = require('../src/app');
+
+// Response should be JSON
+describe('POST /api/v1/lookup', () => {
+  it('responds with a 500 error with no POST data', (done) => {
+    request(app)
+      .post('/api/v1/lookup')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .end(done);
+  });
+  it('responds with a json message', (done) => {
+    request(app)
+      .post('/api/v1/lookup')
+      .send({name: 'jquery',version:'3.5.1'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(done);
+  });
+});
